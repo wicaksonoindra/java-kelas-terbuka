9343ce48cc38e8ef661d7e80a95684fe4281fe23
wicaksonoindra <wicaksonoindra66@gmail.com>
2023-08-09 14:02:11 +0700

    day 4

--- java27-while-loop/src/com/tutorial/Main.java
+++ java27-while-loop/src/com/tutorial/Main.java
@@ -4,7 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        int a = 0;
+        boolean kondisi = true;
+
+        System.out.println("awal program");
+
+        while(kondisi){
+            System.out.println("while loop ke-" + a);
+            a++;
+
+            if (a==10){
+                kondisi = false;
+            }
+        }
+
+        System.out.println("akhir program");
 
     }
 
